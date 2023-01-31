a7610eefa7388828ca2b1c66402868062f6f9dd6
Tobeyyyyy <tobiasscholz2000@gmail.com>
2023-01-31 21:44:54 +0100

    add test

---
+++ tests/cases/compiler/promiseAllBug.ts
@@ -0,0 +1,29 @@
+
+
+// @filename: tsconfig.json
+{
+  "compilerOptions": {
+    "target": "es2016",
+    "module": "commonjs",                               
+    "esModuleInterop": true,                             
+    "forceConsistentCasingInFileNames": true,    
+    "strict": true,                              
+    "skipLibCheck": true,                       
+    "noEmit": true
+  }
+}
+
+// @filename: MyType.ts
+type MyType = {} | null | undefined;
+
+const myVar: MyType = null as MyType;
+
+myVar?.toLocaleString;
+const x = myVar;
+
+// @filename: index.ts
+async function myUnusedFunction() {
+  const fetch1 = Promise.resolve(['hello', 'world']);
+  const [data1] = await Promise.all([fetch1]);
+  data1.length;
+}
