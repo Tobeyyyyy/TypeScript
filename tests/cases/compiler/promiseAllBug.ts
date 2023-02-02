0ed7e8252b8543d3a5ef3cf9983cbbed57df83e5
Tobeyyyyy <tobiasscholz2000@gmail.com>
2023-02-02 20:52:18 +0100

    test

--- tests/cases/compiler/promiseAllBug.ts
+++ tests/cases/compiler/promiseAllBug.ts
@@ -25,6 +25,6 @@ const x = myVar;
 async function myUnusedFunction() {
   const fetch1 = Promise.resolve(['hello', 'world']);
   const [data1] = await Promise.all([fetch1]);
-  data1.length; abc abc abc abc
+  data1.length; abc abc abc abc abc
 } 
 
